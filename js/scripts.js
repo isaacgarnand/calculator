3e57fd555ca8b71801f85ff5e0def99d68ccfce1
Isaac Garnand and Sommer Howe <isaac@>
2020-01-15 14:43:14 -0800

    added subtraction/division & multiplication functions & forms

--- js/scripts.js
+++ js/scripts.js
@@ -31,14 +31,34 @@ var F2C = function(number1) {
 }
 
 //alert(F2C(number1, number2));
-	
-$(document).ready(function() {   
+	$(function() {   
 	$("form#add").submit(function(event) {  
-		event.preventDefault();                                              
+		event.preventDefault();                    
 		var number1 = parseInt($("#add1").val());
 		var number2 = parseInt($("#add2").val());
 		var result = add(number1, number2);
-		$("#output").text(result);
+		$("#output0").text(result);
+		});
+	$("form#subtract").submit(function(event) {  
+		event.preventDefault();                    
+		var number1 = parseInt($("#sub1").val());
+		var number2 = parseInt($("#sub2").val());
+		var result2 = subtract(number1, number2);
+		$("#output1").text(result2);
+		});
+	
+	$("form#multiply").submit(function(event) {  
+		event.preventDefault();                    
+		var number1 = parseInt($("#mult1").val());
+		var number2 = parseInt($("#mult2").val());
+		var result3 = multiply(number1, number2);
+		$("#output2").text(result3);
+		});
+	$("form#divide").submit(function(event) {  
+		event.preventDefault();                    
+		var number1 = parseInt($("#div1").val());
+		var number2 = parseInt($("#div2").val());
+		var result4 = divide(number1, number2);
+		$("#output3").text(result4);
 		});
 	});
-
